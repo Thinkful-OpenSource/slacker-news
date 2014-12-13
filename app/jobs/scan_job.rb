e381f7c467a13889e1e02f41e09024575c043993
SnareChops <snare_chops@yahoo.com>
2014-12-13 02:26:50 +0000

    Adding figaro

--- app/jobs/scan_job.rb
+++ app/jobs/scan_job.rb
@@ -1,7 +1,14 @@
+require 'net/http'
 class ScanJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
     # Scan slack for links and schedule a Scrape job
+    byebug
+    uri = URI("https://slack.com/api/search.messages")
+    params = {token: ENV["slack_api_token"], query: args[0]}
+    uri.query = URI.encode_www_form(params)
+    
+    response = Net::HTTP.get_reponse(uri)
   end
 end
