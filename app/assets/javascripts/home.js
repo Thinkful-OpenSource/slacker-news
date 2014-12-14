7bf983455505dee6b41bcdfe8d3906c8a7190f9b
aric87 <aric87@comcast.net>
2014-12-14 04:14:37 +0000

    added footer controller

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -12,15 +12,18 @@ angular.module('slacker', ['ngMaterial'])
     function success(data, status, headers, config){
       console.log('success');
       $scope.links = data.links;
-    };
+    }
     function error(data, status, headers, config){
       console.log('error');
       $scope.error = "Unable to search at this time";
-    };
+    }
     $http.get('/search.json', {term: term}).success(success).error(error);
   };
   
-  $scope.addLink = function ($event) {
+  
+}])
+.controller('footerController', ['$scope', '$http', '$mdToast', '$mdDialog', function($scope, $http, $mdToast, $mdDialog){
+$scope.addLink = function ($event) {
     $mdDialog.show({
       targetEvent: $event,
       templateUrl: 'add-link.html',
@@ -53,50 +56,6 @@ $(function(){
       words.push(keywords[i].name);
     }
     return words.join(', ');
-  }
-  
-//   $('input').keyup(function(){
-//     if ($(this).val().length > 2){
-//       var term = $(this).val();
-//       $.get('/search.json', {term:term}, function (data) {
-//         var $list = $('#searchResults > ul'), i=0, links = data.links;
-//         $list.empty();
-//         for(i=0;i<links.length;i++){
-//           $list.append(
-//             $('<li/>').attr('class', 'result').append(
-//               $('<a/>').attr('href', links[i].url).html(links[i].title),
-//               $('<br>'),
-//               $('<p/>').html(commifyKeywords(links[i].keywords)),
-//               $('<a/>').attr('class', 'moreInfo').attr('href', '/link/' + links[i].id).html('Show More')
-//             )
-//           );
-//         }
-//       });
-//    }
-//   });
-  var addLink = $('#addLink');
-  
-  addLink.mouseenter(function(){
-    $(this).html("Add new Link");
-  })
-  addLink.mouseleave(function(){
-    $(this).html("+");
-  })
-//   addLink.click(function(){
-//     $('#addLinkForm').show();
-//   })
-  $('#addLinkForm p').click(function(){
-    $('#addLinkForm').hide();
-  })
-  $('#linkAddSubmit').click(function(){
-    var newLink = {
-      
-    }
-    $.ajax({
-        url: '/link',
-        type: 'POST',
-        data: {link: newLink}
-      });
-  })
-  
+  } 
 });
+
