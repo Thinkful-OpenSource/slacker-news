9aa6ea97958e584434dbe7c9d89491532a84500d
SnareChops <snare_chops@yahoo.com>
2015-02-27 00:50:44 +0000

    Upping version in setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ if [ -z ${1+x} ]
     echo "Slack API key required for automatic setup. Please run the command again with the key or contact SnareChops in Slack to obtain the key.";
     exit 1;
 fi
-parts install ruby2.1
-echo 'ruby-2.1.5' > ~/.ruby-version
+parts install ruby2.2
+echo 'ruby-2.2.0' > ~/.ruby-version
 echo "slack_api_token: $1" > config/application.yml
 git remote add upstream https://github.com/Thinkful/slacker-news
 git pull upstream master
